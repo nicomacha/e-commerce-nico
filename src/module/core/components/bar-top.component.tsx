3a80d551e9c740bc2258b847db586d21a744169f
Jona <jonathan.gonzalez@gm2dev.com>
2024-10-17 19:26:04 -0300

    feat: add new icon to close and add state internal

--- src/module/core/components/bar-top.component.tsx
+++ src/module/core/components/bar-top.component.tsx
@@ -1,15 +1,36 @@
-import { styled } from "@mui/material";
-interface BarTop {text:string}
-export const BarTop = ({text}:BarTop) => {
-    
-  return <Container>{text}</Container>;
+import { useState } from 'react';
+import { styled } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+interface BarTop {
+  text: string;
+}
+
+export const BarTop = ({ text }: BarTop) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  if (!isOpen) return null;
+
+  return (
+    <Container>
+      {text}{' '}
+      <CloseIcon
+        onClick={() => setIsOpen(false)}
+        style={{
+          marginLeft: 20,
+          cursor: 'pointer',
+        }}
+      />
+    </Container>
+  );
 };
 
-const Container = styled("div")({
-  backgroundColor: "black",
-  color: "white",
-  height: 38, 
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
+//Poner estilos del icono en un componente custom
+
+const Container = styled('div')({
+  backgroundColor: 'black',
+  color: 'white',
+  height: 38,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
 });
