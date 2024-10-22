7af61db0c006f98572ed314db92991feb5adf38c
Jona <jonathang@hotmail.com.ar>
2024-10-22 18:41:50 -0300

    refactor: update LayoutBase component to accept JSX.Element or React.ReactNode

--- src/modules/features/layout/layout-base.component.tsx
+++ src/modules/features/layout/layout-base.component.tsx
@@ -1,12 +1,14 @@
-import { BarTop } from "../../core/components/bar-top.component";
-import { Header } from "../../core/components/header.component";
+import { BarTop } from '../../core/components/bar-top.component';
+import { Header } from '../../core/components/header.component';
+
 interface LayoutBase {
-  children: string;
+  children: JSX.Element | React.ReactNode;
 }
+
 export const LayoutBase = ({ children }: LayoutBase) => {
   return (
     <div>
-      <BarTop text="Sign up and get 20% off to your first order. Sign Up Now" />
+      <BarTop text='Sign up and get 20% off to your first order. Sign Up Now' />
       <Header />
       {children}
     </div>
