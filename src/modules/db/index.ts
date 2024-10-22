5b874740695d931d1bc6e823b12904e2718f7d27
Jona <jonathang@hotmail.com.ar>
2024-10-22 19:02:08 -0300

    feat: add product data with images and details to the database module

---
+++ src/modules/db/index.ts
@@ -0,0 +1,31 @@
+import maradonaRemera from '../assets/dorsal.maradona.jpg';
+
+export const products = [
+  {
+    id: 1,
+    category: 'remeras',
+    imagen: maradonaRemera,
+    price: 123,
+    rating: 1,
+    stock: 1,
+    title: 'Remera God Maradona',
+  },
+  {
+    id: 2,
+    category: 'pantalon',
+    imagen: maradonaRemera,
+    price: 123,
+    rating: 1,
+    stock: 1,
+    title: 'Pantalon God Remera',
+  },
+  {
+    id: 3,
+    category: 'remeras',
+    imagen: maradonaRemera,
+    price: 123,
+    rating: 1,
+    stock: 1,
+    title: 'Remera God Maradona 2',
+  },
+];
